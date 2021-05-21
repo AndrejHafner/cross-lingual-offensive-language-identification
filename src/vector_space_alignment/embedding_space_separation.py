2bbfd5f8960831d2ef2b0b3bc073b42603e423c4
TURBOPC\ursau <uz2273@student.uni-lj.si>
2021-05-21 14:54:11 +0200

    Separated training and testing

--- src/vector_space_alignment/embedding_space_separation.py
+++ src/vector_space_alignment/embedding_space_separation.py
@@ -6,9 +6,6 @@ import pandas as pd
 import json
 import fasttext.util
 
-from sklearn import svm
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
-from xgboost import XGBRegressor
 
 from src.utils import remove_emojies
 from src.scripts.filter_toxic_comments_dataset import balance_toxic_comment_dataset
@@ -123,7 +120,7 @@ def find_best_combination(train_data, model, probabilities):
     average = np.array([0., 0.])
     nr_words = np.array([0, 0])
     for sent, label in train_data[['content', 'type']].values:
-        words = model.get_line(sent)[0]
+        words = model.get_line(sent.replace("\n", ""))[0]
         probs = np.array([])
         for word in words:
             word = word.lower().strip(',.?!"\'-~')
@@ -215,7 +212,7 @@ def predict_new(ft_model, reg_model, sentences, lang='eng', W=None, normalize=Fa
     known_prob = {}
 
     for sent in sentences:
-        words = ft_model.get_line(sent)[0]
+        words = ft_model.get_line(sent.replace("\n", ""))[0]
         unknown_words = []
         probs = []
         for word in words:
@@ -267,169 +264,4 @@ def predict_new(ft_model, reg_model, sentences, lang='eng', W=None, normalize=Fa
         predictions.append(sent_prob)
 
     return np.array(predictions)
-
-
-if __name__ == '__main__':
-
-    ft_en = fasttext.load_model('../data/fasttext_models/wiki.en.bin')
-    print('Model loaded')
-
-    dataset = 'toxic'
-
-    # making embeddings and their target probabilities
-    X, y = make_embeddings_and_target(ft_en, dataset)
-
-    # normalize embedding vectors to length 1 (normalizing rows - axis 1)
-    X_norm = (X.T/np.linalg.norm(X, axis=1)).T
-
-    svm_prob_predictor = svm.SVR(kernel='rbf')
-    print('Fitting SVM')
-    svm_prob_predictor.fit(X_norm, y)
-    with open(f'../data/SVM_prob_predictor_toxic_norm_rbf.pickle', 'wb') as f:
-        pickle.dump(svm_prob_predictor, f)
-
-    # with open(f'../data/SVM_prob_predictor_gab_4.pickle', 'rb') as f:
-    #     svm_prob_predictor = pickle.load(f)
-
-    # xgb_predictor = XGBRegressor()
-    # print('Fitting XGB')
-    # xgb_predictor.fit(X, y)
-    # with open(f'../data/XGB_prob_predictor_reddit.pickle', 'wb') as f:
-    #     pickle.dump(xgb_predictor, f)
-
-    print('Testing')
-
-    test = pd.read_csv(f'../data/datasets/{dataset}/test.csv')
-
-    y_test = test['type'].values
-    sentences = test['content'].values
-
-    y_predictions = predict_new(ft_en, svm_prob_predictor, sentences, 'eng', None, True)
-    # y_predictions = predict_new(ft_en, xgb_predictor, sentences, 4)
-
-    y_bin = y_predictions > 0.5
-
-    print(f'Accuracy: {accuracy_score(y_test, y_bin)}')
-    print(f'Precision: {precision_score(y_test, y_bin)}')
-    print(f'Recall: {recall_score(y_test, y_bin)}')
-    print(f'F1 score: {f1_score(y_test, y_bin)}')
-
-    stop = 0
-
-    # GAB data, wiki
-    # Average
-    # probability
-    # of
-    # all
-    # words in train
-    # set: 0.5678504185033753
-    # Average: [0.56785042 0.56785042], number: [328346 328346]
-    # Mean:
-    # 0.881173255321606
-    # 0.7951422302629046
-    # Logit:
-    # 0.8994187613072097
-    # 0.703991685592209
-    # Product:
-    # 0.648138881404211
-    # Product
-    # inverted:
-    # 0.5432849609299819
-    # Fitting
-    # SVM
-    # Testing
-    # Accuracy: 0.8489607390300231
-    # Precision: 0.8834555827220864
-    # Recall: 0.757247642333217
-    # F1
-    # score: 0.8154974609742336
-    #
-    # Fitting SVM rbf norm
-    # Testing
-    # Accuracy: 0.7832178598922248
-    # Precision: 0.6991513824254038
-    # Recall: 0.8920712539294446
-    # F1 score: 0.783916513198281
-    #
-    # Fitting SVM - polynomial (3)
-    # Testing
-    # Accuracy: 0.8532717474980754
-    # Precision: 0.9081196581196581
-    # Recall: 0.7422284317149843
-    # F1 score: 0.8168364405150874
-
-    # Fitting SVM - linear
-    # Testing
-    # Accuracy: 0.7578137028483449
-    # Precision: 0.9504189944134078
-    # Recall: 0.47537548026545584
-    # F1 score: 0.6337601862630967
-    # Fitting SVM - linear NORM
-    # Testing
-    # Accuracy: 0.789838337182448
-    # Precision: 0.8410746812386156
-    # Recall: 0.6451274886482711
-    # F1 score: 0.7301838307966001
-
-    # Fitting SVM - quad
-    # Testing
-    # Accuracy: 0.85635103926097
-    # Precision: 0.8796223446105429
-    # Recall: 0.7809989521480964
-    # F1 score: 0.8273820536540241
-    # Fitting SVM - 4
-    # Testing
-    # Accuracy: 0.828175519630485
-    # Precision: 0.9296606000983768
-    # Recall: 0.6601466992665037
-    # F1 score: 0.7720588235294119
-    # _____________________________
-    # Fitting XGB
-    # Testing
-    # Accuracy: 0.7862971516551194
-    # Precision: 0.8261831048208758
-    # Recall: 0.6524624519734544
-    # F1 score: 0.7291178766588603
-
-    # REDDIT data, wiki
-    # Average
-    # probability
-    # of
-    # all
-    # words in train
-    # set: 0.5697244711031356
-    # Average: [0.56972447 0.56972447], number: [212431 212431]
-    # Mean:
-    # 0.6136312471859523
-    # 0.8861999099504727
-    # Logit:
-    # 0.7701485817199459
-    # 0.8044236830256641
-    # Product:
-    # 0.8129783881134625
-    # Product
-    # inverted:
-    # 0.7619878433138226
-    # Fitting
-    # SVM
-    # Testing
-    # Accuracy: 0.8815848716794237
-    # Precision: 0.8418430884184309
-    # Recall: 0.6288372093023256
-    # F1
-    # score: 0.7199148029818956
-    #
-    # Fitting SVM - poly 2
-    # Testing
-    # Accuracy: 0.8624493471409275
-    # Precision: 0.8659305993690851
-    # Recall: 0.5106976744186047
-    # F1 score: 0.6424809830310123
-    # ______________________________
-    # Fitting XGB
-    # Testing
-    # Accuracy: 0.7793786582620441
-    # Precision: 0.88
-    # Recall: 0.10232558139534884
-    # F1 score: 0.18333333333333332
 
---
+++ src/vector_space_alignment/monolingual_train.py
@@ -0,0 +1,46 @@
+import fasttext
+import numpy as np
+import pickle
+from sklearn import svm
+from src.vector_space_alignment.embedding_space_separation import make_embeddings_and_target
+
+
+if __name__ == '__main__':
+
+    # choose dataset and set parameters for training
+    dataset = 'toxic'
+    normalize = True
+    kernel = 'poly'
+    degree = 2
+
+    ft_en = fasttext.load_model('../data/fasttext_models/wiki.en.bin')
+    print('Model loaded')
+
+    # making embeddings and their target probabilities
+    X, y = make_embeddings_and_target(ft_en, dataset)
+
+    # normalize embedding vectors to length 1 (normalizing rows - axis 1)
+    if normalize:
+        X = (X.T/np.linalg.norm(X, axis=1)).T
+
+    # fitting svm
+    print('Fitting SVM')
+    svm_prob_predictor = svm.SVR(kernel=kernel, degree=degree)
+    svm_prob_predictor.fit(X, y)
+
+    norm_tag = 'norm_' if normalize else ''
+    kernel_tag = f'{degree}' if kernel == 'poly' else kernel
+
+    # saving model
+    with open(f'../data/SVM_prob_predictor_{dataset}_{norm_tag}{kernel_tag}.pickle', 'wb') as f:
+        pickle.dump(svm_prob_predictor, f)
+
+    # VERJETNO SAM ZA ZBRISAT, za vsak slucaj
+    # with open(f'../data/SVM_prob_predictor_gab_4.pickle', 'rb') as f:
+    #     svm_prob_predictor = pickle.load(f)
+
+    # xgb_predictor = XGBRegressor()
+    # print('Fitting XGB')
+    # xgb_predictor.fit(X, y)
+    # with open(f'../data/XGB_prob_predictor_reddit.pickle', 'wb') as f:
+    #     pickle.dump(xgb_predictor, f)
